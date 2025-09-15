42cb5a19d9c5020b8ad4ec5552be471309ff03f0
murilo <57834900+muriloonunes@users.noreply.github.com>
2025-09-14 22:56:52 -0300

    v1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.mno.jamscope"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.2.0"
+        versionCode = 13
+        versionName = "1.2.1"
 
         vectorDrawables.useSupportLibrary = true
 
